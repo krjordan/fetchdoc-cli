7feb60db7a2905364d34a2baef9dde1f15877cb0
Ryan Jordan <ryjordan@gmail.com>
2023-09-04 17:26:27 -0500

    docs(src): add JSDoc comments

--- src/spawnWrapper.ts
+++ src/spawnWrapper.ts
@@ -1,5 +1,13 @@
 import { spawn as cpSpawn, SpawnOptions, ChildProcess } from 'child_process'
 
+/**
+ * Spawns a child process with the given command, arguments, and options.
+ *
+ * @param {string} command - The command to execute.
+ * @param {ReadonlyArray<string>} args - The arguments to pass to the command (default: []).
+ * @param {SpawnOptions} options - The options for spawning the child process.
+ * @return {ChildProcess} - The spawned child process.
+ */
 export function spawn(
 	command: string,
 	args: ReadonlyArray<string> = [],
