76bf775b95b985d866741e8988561c2637df015c
Ryan Jordan <ryjordan@gmail.com>
2023-09-05 01:17:12 +0000

    fix(rollup): add rollup to fix broken package (#5)
    * fix(rollup): add rollup to fix broken package
    
    * chore(build): remove second CodeQL instance
    
    * test(mocha): attempt to fix tests
    
    * fix(test): fix test files not being recognized
    
    * styles(codacy): fix code style
    
    ---------
    
    Co-authored-by: Ryan Jordan <ryanj@localhost.localdomain>

---
+++ rollup.config.mjs
@@ -0,0 +1,25 @@
+import typescript from '@rollup/plugin-typescript'
+import pkg from './package.json' assert { type: 'json' }
+import json from '@rollup/plugin-json'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+
+export default {
+	input: 'src/index.ts',
+	output: {
+		dir: 'dist',
+		format: 'cjs',
+		banner: '#!/usr/bin/env node',
+	},
+	external: ['tslib', 'child_process', 'axios'],
+	plugins: [
+		typescript({ exclude: 'node_modules' }),
+		nodeResolve({ exportConditions: ['node'], preferBuiltins: false }),
+		commonjs({ includes: 'node_modules/**' }),
+		json(),
+		replace({
+			VERSION: JSON.stringify(pkg.version),
+		}),
+	],
+}
